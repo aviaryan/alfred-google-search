34c5153b8e55976a25e85cc3ff7f1139a20c2c85
Avi Aryan <avi.aryan123@gmail.com>
2017-09-07 15:49:20 +0530

    cache to improve copy speed after adding $
    suffers from unicode hell again

--- src/script.py
+++ src/script.py
@@ -2,13 +2,15 @@
 
 from __future__ import print_function
 
-from sys import argv, stdout
+from sys import argv, stdout, version
 import json
+import os
 
 # https://github.com/aviaryan/googlesearch
 from gsearch.googlesearch import search
 
 
+isPython2 = version.startswith('2')
 isClipboard = False
 
 
@@ -45,16 +47,46 @@ def main():
     if query.endswith('$'):
         query = query[:-1]
         isClipboard = True
+        # load cache
+        if os.path.isfile('cache.txt'):
+            if isPython2:
+                fp = open('cache.txt', 'r')
+                data = fp.read().decode('utf-8', errors='ignore')
+                query = query.decode('utf-8', errors='ignore')  # startswith wants this in unicode, since data in unicode
+            else:
+                fp = open('cache.txt', 'r', encoding='utf-8')
+                data = fp.read()
+            fp.close()
+            if data.startswith(query + '\n'):
+                obj = json.loads(data[len(query)+1:])
+                for r in obj['items']:
+                    if not r['arg'].endswith('$'):  # case: prev $ too
+                        r['arg'] = r['arg'] + '$'
+                out = json.dumps(obj, indent=4, encoding='utf-8')
+                # weird bug
+                out += '\n'
+                return out
     results = search(query, num_results=10)
     items = []
     if len(results) == 0:
         items.append(makeItem(query, 'Please try again after 20 seconds', 'RATE LIMIT EXCEEDED'))
     for r in results:
         items.append(makeItem(query, r[1], r[0]))
-    return makeReturn(items)
+    # return back
+    out = makeReturn(items)
+    out = json.dumps(out, indent=4, encoding='utf-8')
+    # caching -- helps when $ inserted at last and you don't want to search again
+    if isPython2:
+        fp = open('cache.txt', 'w')
+    else:
+        fp = open('cache.txt', 'w', encoding='utf-8')
+    fp.write(query + '\n' + out)
+    fp.close()
+
+    return out
 
 
 
 if __name__ == '__main__':
     data = main()
-    stdout.write(json.dumps(data, indent=4, encoding='utf-8'))
+    stdout.write(data)
